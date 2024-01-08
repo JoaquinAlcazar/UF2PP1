52ec3814242a0bceec3327bc73b0f574dfdc76ee
argo <argo@MAQUETA>
2024-01-08 12:41:40 +0100

    Add project files.

---
+++ ClassLibrary1/Class1.cs
@@ -0,0 +1,80 @@
+﻿using System.Data;
+using System.IO.Pipes;
+using System.Runtime.CompilerServices;
+
+namespace ClassLibraryPP1
+{
+    public class programMethods
+    {
+        public static bool oddTester()
+        {
+            int input;
+            const string ask = "Inserta un nombre per comprobar si es senar";
+
+            Console.WriteLine(ask);
+            input = Convert.ToInt32(Console.ReadLine());
+            if (input%2 == 0)
+            {
+                Console.WriteLine("No es un nombre senar");
+                return false;
+            }
+            Console.WriteLine("Es un nombre senar");
+            return true;
+        }
+        
+        public static void powMaker()
+        {
+            int basse;
+            int exponent;
+            int result = 1;
+            const string baseAsk = "Introdueix la base";
+            const string expAsk = "Introdueix l'exponent";
+
+            Console.WriteLine(baseAsk);
+            basse = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine(expAsk);
+            exponent = Convert.ToInt32(Console.ReadLine());
+
+            for (int i = 0; i < exponent; i++) 
+            {
+                result = result * basse;
+            }
+            Console.WriteLine($"El resultat de la potencia es {result}");
+        }
+
+        public static int randomGenerator()
+        {
+            Random RNG = new Random();
+            return RNG.Next();
+        }
+
+        public static void vowelConsonantCounter()
+        {
+            Console.WriteLine("Introdueix un text");
+            int consonantCount = 0;
+            int vowelCount = 0;
+            string stringInput = " ";
+            stringInput = Console.ReadLine();
+            stringInput = stringInput.ToLower();
+
+            foreach (char letter in stringInput)
+            {
+                char actualLetter = letter;
+                if (actualLetter != ' ')
+                {
+                    if (actualLetter == 'a' || actualLetter == 'e' || actualLetter == 'i' || actualLetter == 'o' || actualLetter == 'u')
+                    {
+                        vowelCount++;
+                    }
+                    else
+                    {
+                        consonantCount++;
+                    }
+                }
+            }
+
+            Console.WriteLine($"Vocals: {vowelCount}. Consonants: {consonantCount}");
+        }
+    }
+}
---
+++ UF2PP1/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using ClassLibraryPP1;
+
+namespace ProvaPractica
+{
+    class M03UF2Ex2
+    {
+        static void Main(string[] args)
+        {
+            const string mainMenu = "Selecciona una de les seguents opcions \n" +
+                "a.Validar si un nombre es senar \n" +
+                "b.Calcular la potencia d'un nombre \n" +
+                "c.Retornar un valor aleatori\n" +
+                "d.Comptar el nombre de vocals o consonants en un text\n" +
+                "e.Sortir";
+            const string charFail = "Carácter no valid, introdueix altre";
+
+            string optionSelectSt;
+            char optionSelect = ' ';
+
+            while (optionSelect != 'e')
+            {
+                Console.WriteLine(mainMenu);
+                optionSelectSt = Console.ReadLine();
+                optionSelect = Convert.ToChar(optionSelectSt.ToLower());
+
+                switch (optionSelect)
+                {
+                    case 'a':
+                        programMethods.oddTester();
+                        break;
+
+                    case 'b':
+                        programMethods.powMaker();
+                        break;
+                    case 'c':
+                        Console.WriteLine($"Numero aleatori: {programMethods.randomGenerator()}");
+                        break;
+                    case 'd':
+                        programMethods.vowelConsonantCounter();
+                        break;
+                    case 'e':
+                        Console.WriteLine("Gracies per utilizar la aplicacio");
+                        break;
+                    default:
+                        Console.WriteLine(charFail);
+                        break;
+                }
+            }
+        }          
+    }
+}
